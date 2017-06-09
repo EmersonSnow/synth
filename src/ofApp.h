2da2dff710bf788d975cfc037b0863255941770b
EmersonSnow <zelm.snow@gmail.com>
2017-06-09 19:11:00 +0100

    Continue working the MidiController class, deciding the format

--- src/ofApp.h
+++ src/ofApp.h
@@ -698,13 +698,13 @@ private:
     
 };
 
-struct PeakSegment
+struct SegmentMidi
 {
     float duration;
     float volumeStart;
     float volumeEnd;
     float curve;
-    PeakSegment * previousPeakSegment;
+    //SegmentMidi * previousSegment;
 };
 class MidiController : public ofxMidiListener
 {
@@ -747,13 +747,77 @@ class MidiController : public ofxMidiListener
         {
             return waveType;
         }
-        void setDuration(float durationTotal, float volumePeak);
-        void getDuration();
-        void setRepeat(bool repeat);
-        void setAttack(float duration, float startVolume, float curve);
-        void addPeakSegent();
-        void setDecay(float duration, float volumeEnd, float curve);
-        void setCutoff(float duration, float volumeEnd, float curve);
+        void setDuration(float durationTotal, float volumePeak)
+        {
+            this->durationTotal = durationTotal;
+            this->volumePeak = volumePeak;
+        }
+        void getDuration()
+        {
+            return durationTotal;
+        }
+        void setRepeat(bool repeat)
+        {
+            bRepeat = repeat;
+        }
+        void getRepeat()
+        {
+            return bRepeat;
+        }
+        void setAttack(float duration, float volumeEnd, float curve)
+        {
+            durationAttack = duration;
+            volumeAttackEnd = volumeEnd;
+            curveAttack = curve;
+        }
+        void setDecay(float duration, float volumeEnd, float curve)
+        {
+            durationDecay = duration;
+            volumeDecayEnd = volumeEnd;
+            curveDecay = curve;
+        }
+        void setCutoff(float duration, float volumeEnd, float curve)
+        {
+            durationCutoff = duration;
+            volumeCutoffEnd = volumeEnd;
+            curveCutoff = curve;
+        }
+        void addPeakSegnent(float duration, float volumeEnd, float curve)
+        {
+            SegmentMidi temp;
+            temp.duration = duration;
+            temp.volumeEnd = volumeEnd;
+            temp.curve = curve;
+            if (segmentsPeak.size() == 0)
+            {
+                temp.volumeStart = volumePeak;
+            } else
+            {
+                temp.volumeEnd = volumeEnd;
+            }
+            segmentsPeak.push_back(temp);
+            
+            bPeakSegmentsChanged = true;
+            
+        }
+        void replacePeakSegment(int index, float duration, float volumeEnd, float curve)
+        {
+            if (segmentsPeak.size() <= index)
+                return;
+            segmentsPeak[index].duration = duration;
+            segmentsPeak[index].volumeEnd = volumeEnd;
+            segmentsPeak[index].curve = curve;
+            
+            bPeakSegmentsChanged = true;
+        }
+        void removePeakSegment();
+        void generateSynth()
+        {
+            if (bPeakSegmentsChanged)
+            {
+                
+            }
+        }
         void newMidiMessage(ofxMidiMessage& message)
         {
             currentMidiMessage = message;
@@ -779,21 +843,30 @@ class MidiController : public ofxMidiListener
     
         WaveType waveType;
         int sampleRate;
-        bool bRepeating;
+        bool bRepeat;
     
     
+        //bool bDurationChanged;
         float durationTotal;
         float volumePeak;
     
+        //bool bAttackChanged;
         float durationAttack;
         float volumeAttackStart;
+        float curveAttack;
     
+        //bool bDecayChanged;
         float durationDecay;
         float volumeDecayEnd;
+        float curveDecay;
     
+        //bool bCutoffChanged;
         float durationCutoff;
         float volumeCutoffEnd;
+        float curveCutoff;
     
+        bool bPeakSegmentsChanged = false;
+        vector<SegmentMidi> segmentsPeak;
     
         zelmAudioMixer audioMixer;
         //vector<ToneGenerator3> toneGenerators;
